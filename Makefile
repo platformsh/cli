c632f4672f7e356c445b36fe673bfddf6f47d205
Antonis Kalipetis <antonis.kalipetis@platform.sh>
2022-08-04 17:36:16 +0300

    Fix macOS building to work on any architecture

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 PHP_VERSION = 8.0.21
-PLATFORM := $(shell uname -s |  | tr '[:upper:]' '[:lower:]')
+PLATFORM := $(shell uname -s | tr '[:upper:]' '[:lower:]')
+PHP_BINARY_PATH := legacy/archives/php_$(PLATFORM)_$(uname -m)
 
 php-linux:
 	cp ext/extensions.txt ext/static-php-cli/docker
@@ -10,12 +11,12 @@ php-linux:
 	mv -f legacy/archives/php legacy/archives/php_linux_`uname -m`
 	rm -f legacy/archives/micro.sfx
 
-php-darwin:
-	bash build-php-macos.sh $(PHP_VERSION)
-	mv -f php-$(PHP_VERSION)/sapi/cli/php legacy/archives/php_$(PLATFORM)_`uname -m`
-	rm -rf php-$(PHP_VERSION)
+legacy/archives/php_darwin_$(uname -m):
+	bash build-php-macos.sh $(PHP_VERSION) $(PLATFORM)
+	mv -f $(PLATFORM)/php-$(PHP_VERSION)/sapi/cli/php $(PHP_BINARY_PATH)
+	rm -rf $(PLATFORM)
 
-php: php-$(PLATFORM)
+php: $(PHP_BINARY_PATH)
 
 snapshot:
 	goreleaser build --snapshot --rm-dist
--- build-php-macos.sh
+++ build-php-macos.sh
@@ -1,22 +1,29 @@
 set -ex
 
-curl -fSsl https://www.php.net/distributions/php-$1.tar.gz | tar xzf -
-cd php-$1
+BREW_PREFIX=$(brew --prefix)
+export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$BREW_PREFIX/opt/openssl@1.1/lib/pkgconfig"
+
+DIR=$2
+mkdir -p $DIR
+curl -fSsl https://www.php.net/distributions/php-$1.tar.gz | tar  xzf - -C $DIR
+cd $DIR/php-$1
+
 rm -f sapi/cli/php
 
-export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:/opt/homebrew/opt/openssl@1.1/lib/pkgconfig"
 ./buildconf --force
 ./configure \
+  --disable-shared \
   --enable-embed=static \
   --enable-mbstring \
+  --enable-pcntl \
   --enable-phar \
-  --enable-static=yes \
+  --enable-posix \
+  --enable-static \
   --enable-sysvmsg \
   --with-openssl \
-  --enable-pcntl \
-  --enable-posix \
-  --with-pcre-jit \
+  --with-pear=no \
+  --without-pcre-jit \
   --disable-all
 
-export PATH="/opt/homebrew/opt/bison/bin:$PATH"
+export PATH="$BREW_PREFIX/opt/bison/bin:$PATH"
 make -j$(sysctl -n hw.logicalcpu) cli
