38cf08f336c516e843c8490145207a01ba41aecb
Patrick Dawkins <patrick@platform.sh>
2023-09-22 11:03:49 +0100

    Avoid notifying about updates on every run
    Also applies config to where the state is stored instead of a hardcoded path

---
+++ internal/state/state.go
@@ -0,0 +1,57 @@
+package state
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+
+	"github.com/platformsh/cli/internal/config"
+)
+
+type State struct {
+	Updates struct {
+		LastChecked int64 `json:"last_checked"`
+	} `json:"updates"`
+}
+
+// Load reads state from the filesystem.
+func Load(cnf *config.Config) (state State, err error) {
+	statePath, err := getPath(cnf)
+	if err != nil {
+		return
+	}
+	data, err := os.ReadFile(statePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			err = nil
+		}
+		return
+	}
+	err = json.Unmarshal(data, &state)
+	return
+}
+
+// Save writes state to the filesystem.
+func Save(state State, cnf *config.Config) error {
+	statePath, err := getPath(cnf)
+	if err != nil {
+		return err
+	}
+
+	data, err := json.Marshal(state)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(statePath, data, 0o600)
+}
+
+// getPath determines the path to the state JSON file depending on config.
+func getPath(cnf *config.Config) (string, error) {
+	writableDir, err := cnf.WritableUserDir()
+	if err != nil {
+		return "", err
+	}
+
+	return filepath.Join(writableDir, cnf.Application.UserStateFile), nil
+}
