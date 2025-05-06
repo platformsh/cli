0897e3c8c19f8d6ea70246e9e7b088c861614339
Patrick Dawkins <patrick@platform.sh>
2025-05-06 20:02:30 +0100

    Simplify list commands

--- commands/list_models.go
+++ commands/list_models.go
@@ -4,14 +4,11 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"math"
-	"regexp"
 	"sort"
 	"strings"
 	"text/tabwriter"
 
 	"github.com/fatih/color"
-	orderedmap "github.com/wk8/go-ordered-map/v2"
 
 	"github.com/platformsh/cli/internal/config"
 )
@@ -39,29 +36,14 @@ func innerProjectInitCommand(cnf *config.Config) Command {
 			},
 		},
 		Definition: Definition{
-			Arguments: &orderedmap.OrderedMap[string, Argument]{},
-			Options: orderedmap.New[string, Option](orderedmap.WithInitialData[string, Option](
-				orderedmap.Pair[string, Option]{
-					Key:   HelpOption.GetName(),
-					Value: HelpOption,
-				},
-				orderedmap.Pair[string, Option]{
-					Key:   VerboseOption.GetName(),
-					Value: VerboseOption,
-				},
-				orderedmap.Pair[string, Option]{
-					Key:   VersionOption.GetName(),
-					Value: VersionOption,
-				},
-				orderedmap.Pair[string, Option]{
-					Key:   YesOption.GetName(),
-					Value: YesOption,
-				},
-				orderedmap.Pair[string, Option]{
-					Key:   noInteractionOption.GetName(),
-					Value: noInteractionOption,
-				},
-			)),
+			Arguments: []Argument{},
+			Options: []Option{
+				HelpOption,
+				VerboseOption,
+				VersionOption,
+				YesOption,
+				noInteractionOption,
+			},
 		},
 		Hidden: false,
 	}
@@ -90,29 +72,14 @@ func innerAppConfigValidateCommand(cnf *config.Config) Command {
 			},
 		},
 		Definition: Definition{
-			Arguments: &orderedmap.OrderedMap[string, Argument]{},
-			Options: orderedmap.New[string, Option](orderedmap.WithInitialData[string, Option](
-				orderedmap.Pair[string, Option]{
-					Key:   HelpOption.GetName(),
-					Value: HelpOption,
-				},
-				orderedmap.Pair[string, Option]{
-					Key:   VerboseOption.GetName(),
-					Value: VerboseOption,
-				},
-				orderedmap.Pair[string, Option]{
-					Key:   VersionOption.GetName(),
-					Value: VersionOption,
-				},
-				orderedmap.Pair[string, Option]{
-					Key:   YesOption.GetName(),
-					Value: YesOption,
-				},
-				orderedmap.Pair[string, Option]{
-					Key:   noInteractionOption.GetName(),
-					Value: noInteractionOption,
-				},
-			)),
+			Arguments: []Argument{},
+			Options: []Option{
+				HelpOption,
+				VerboseOption,
+				VersionOption,
+				YesOption,
+				noInteractionOption,
+			},
 		},
 		Hidden: false,
 	}
@@ -135,8 +102,8 @@ type Command struct {
 	Name        CommandName `json:"name"`
 	Usage       []string    `json:"usage"`
 	Aliases     []string    `json:"aliases"`
-	Description CleanString `json:"description"`
-	Help        CleanString `json:"help"`
+	Description string      `json:"description"`
+	Help        string      `json:"help"`
 	Examples    []Example   `json:"examples"`
 	Definition  Definition  `json:"definition"`
 	Hidden      bool        `json:"hidden"`
@@ -147,7 +114,7 @@ func (c *Command) HelpPage(cnf *config.Config) string {
 	writer := tabwriter.NewWriter(&b, 0, 8, 1, ' ', 0)
 
 	fmt.Fprintln(writer, color.YellowString("Command: ")+c.Name.String())
-	fmt.Fprintln(writer, color.YellowString("Description: ")+c.Description.String())
+	fmt.Fprintln(writer, color.YellowString("Description: ")+c.Description)
 	fmt.Fprintln(writer, "")
 	if len(c.Usage) > 0 {
 		fmt.Fprintln(writer, color.YellowString("Usage:"))
@@ -156,18 +123,16 @@ func (c *Command) HelpPage(cnf *config.Config) string {
 		}
 		fmt.Fprintln(writer, "")
 	}
-	if c.Definition.Arguments != nil && c.Definition.Arguments.Len() > 0 {
+	if len(c.Definition.Arguments) > 0 {
 		fmt.Fprintln(writer, color.YellowString("Arguments:"))
-		for pair := c.Definition.Arguments.Oldest(); pair != nil; pair = pair.Next() {
-			arg := pair.Value
+		for _, arg := range c.Definition.Arguments {
 			fmt.Fprintf(writer, "  %s\t%s\n", color.GreenString(arg.Name), arg.Description)
 		}
 		fmt.Fprintln(writer, "")
 	}
-	if c.Definition.Options != nil && c.Definition.Options.Len() > 0 {
+	if len(c.Definition.Options) > 0 {
 		fmt.Fprintln(writer, color.YellowString("Options:"))
-		for pair := c.Definition.Options.Oldest(); pair != nil; pair = pair.Next() {
-			opt := pair.Value
+		for _, opt := range c.Definition.Options {
 			shortcut := opt.Shortcut
 			if shortcut == "" {
 				shortcut = "   "
@@ -186,7 +151,7 @@ func (c *Command) HelpPage(cnf *config.Config) string {
 	if len(c.Examples) > 0 {
 		fmt.Fprintln(writer, color.YellowString("Examples:"))
 		for _, example := range c.Examples {
-			fmt.Fprintln(writer, " "+example.Description.String()+":")
+			fmt.Fprintln(writer, " "+example.Description+":")
 			fmt.Fprintln(writer,
 				color.GreenString(fmt.Sprintf("   %s %s %s", cnf.Application.Executable, c.Name.String(), example.Commandline)))
 			fmt.Fprintln(writer, "")
@@ -235,146 +200,37 @@ func (n *CommandName) UnmarshalJSON(text []byte) error {
 	return nil
 }
 
-type CleanString string
-
-func (s CleanString) String() string {
-	return string(s)
-}
-
-func (s *CleanString) MarshalJSON() ([]byte, error) {
-	buff := new(bytes.Buffer)
-	encoder := json.NewEncoder(buff)
-	encoder.SetEscapeHTML(false)
-	err := encoder.Encode(s.String())
-	return buff.Bytes(), err
-}
-
-var (
-	//nolint:lll
-	regexColor = regexp.MustCompile(`<((?:fg=(?P<fg>\w+);?)?(?:bg=(?P<bg>\w+);?)?(?:options=(?P<options>\w+);?)?)?>(?P<label>.*?)</>`)
-	regexTag   = regexp.MustCompile(`<.*?>`)
-)
-
-func (s *CleanString) UnmarshalJSON(text []byte) error {
-	var str string
-	err := json.Unmarshal(text, &str)
-	if err != nil {
-		return err
-	}
-
-	match := regexColor.FindStringSubmatch(str)
-	if len(match) != 0 {
-		res := make(map[string]string)
-		for i, name := range regexColor.SubexpNames() {
-			if i != 0 && name != "" {
-				res[name] = match[i]
-			}
-		}
-
-		atrs := make([]color.Attribute, 0, 2)
-		switch res["fg"] {
-		case "white":
-			atrs = append(atrs, color.FgWhite)
-		case "red":
-			atrs = append(atrs, color.FgRed)
-		case "yellow":
-			atrs = append(atrs, color.FgYellow)
-		}
-		switch res["bg"] {
-		case "white":
-			atrs = append(atrs, color.BgWhite)
-		case "red":
-			atrs = append(atrs, color.BgRed)
-		case "yellow":
-			atrs = append(atrs, color.BgYellow)
-		}
-		colorStr := color.New(atrs...).SprintFunc()
-
-		str = regexColor.ReplaceAllString(str, colorStr(res["label"]))
-	}
-
-	// Remove all remain tags like <comment></comment> and <info></info>
-	str = regexTag.ReplaceAllString(str, "")
-
-	*s = CleanString(str)
-	return nil
-}
-
 type Example struct {
-	Commandline string      `json:"commandline"`
-	Description CleanString `json:"description"`
+	Commandline string `json:"commandline"`
+	Description string `json:"description"`
 }
 
 type Definition struct {
-	Arguments *orderedmap.OrderedMap[string, Argument] `json:"arguments"`
-	Options   *orderedmap.OrderedMap[string, Option]   `json:"options"`
+	Arguments []Argument `json:"arguments"`
+	Options   []Option   `json:"options"`
 }
 
 type Argument struct {
-	Name        string      `json:"name"`
-	IsRequired  YesNo       `json:"is_required"`
-	IsArray     YesNo       `json:"is_array"`
-	Description CleanString `json:"description"`
-	Default     Any         `json:"default"`
+	Name        string `json:"name"`
+	IsRequired  bool   `json:"is_required"`
+	IsArray     bool   `json:"is_array"`
+	Description string `json:"description"`
+	Default     any    `json:"default"`
 }
 
 type Option struct {
-	Name            string      `json:"name"`
-	Shortcut        string      `json:"shortcut"`
-	AcceptValue     YesNo       `json:"accept_value"`
-	IsValueRequired YesNo       `json:"is_value_required"`
-	IsMultiple      YesNo       `json:"is_multiple"`
-	Description     CleanString `json:"description"`
-	Default         Any         `json:"default"`
-	Hidden          bool        `json:"hidden"`
+	Name            string `json:"name"`
+	Shortcut        string `json:"shortcut"`
+	AcceptValue     bool   `json:"accept_value"`
+	IsValueRequired bool   `json:"is_value_required"`
+	IsMultiple      bool   `json:"is_multiple"`
+	Description     string `json:"description"`
+	Default         any    `json:"default"`
+	Hidden          bool   `json:"hidden"`
 }
 
 func (o *Option) GetName() string {
 	return strings.TrimPrefix(o.Name, "--")
-}
-
-type YesNo bool
-
-func (y YesNo) String() string {
-	if y {
-		return "yes"
-	}
-	return "no"
-}
-
-type Any struct {
-	any
-}
-
-func (a *Any) String() string {
-	if a.any == nil {
-		return "NULL"
-	}
-	switch t := a.any.(type) {
-	case bool:
-		return fmt.Sprintf("%t", a.any)
-	case float32, float64:
-		s := a.any.(float64) //nolint:errcheck
-		if s == math.Trunc(s) {
-			return fmt.Sprintf("%d", int64(s))
-		}
-		return fmt.Sprintf("%f", s)
-	case string:
-		s := a.any.(string) //nolint:errcheck
-		return fmt.Sprintf("'%s'", s)
-	case []any, []string, []int, []float64:
-		return "array ()"
-	default:
-		panic(fmt.Sprintf("options: unsupported type: %T", t))
-	}
-}
-
-func (a Any) MarshalJSON() ([]byte, error) {
-	return json.Marshal(a.any)
-}
-
-func (a *Any) UnmarshalJSON(text []byte) error {
-	return json.Unmarshal(text, &a.any)
 }
 
 type Namespace struct {
