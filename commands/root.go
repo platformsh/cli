ad48fc8bcb76829c113da08fb780d6ee8478aafd
Mickael Gaillard <mickael.gaillard@platform.sh>
2025-08-01 18:49:30 +0200

    ClonSun Integration

--- commands/root.go
+++ commands/root.go
@@ -143,6 +143,14 @@ func newRootCommand(cnf *config.Config, assets *vendorization.VendorAssets) *cob
 		fmt.Println(internalCmd.HelpPage(cnf))
 	})
 
+	// Add ClonSun to command
+	cloneCommand := NewCloneProjectCommand()
+	cloneCommand.SetHelpFunc(func(_ *cobra.Command, _ []string) {
+		internalCmd := innerCloneProjectCommand(cnf)
+		fmt.Println(internalCmd.HelpPage(cnf))
+	})
+	// End of ClonSun
+
 	// Add subcommands.
 	cmd.AddCommand(
 		newConfigInstallCommand(),
@@ -152,6 +160,7 @@ func newRootCommand(cnf *config.Config, assets *vendorization.VendorAssets) *cob
 		projectInitCmd,
 		validateCmd,
 		versionCommand,
+		cloneCommand,
 	)
 
 	//nolint:errcheck
